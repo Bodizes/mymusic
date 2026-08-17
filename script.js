56ffe497aba07936bdbe746b0f7c79eca228b6d2
Bodizes <kakashiotako52@gmail.com>
2026-08-16 22:21:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const musicas = [
+    {
+        nome: "Música 1",
+        arquivo: "musicas/musica1.mp3"
+    },
 
+    {
+        nome: "Música 2",
+        arquivo: "musicas/musica2.mp3"
+    },
+
+    {
+        nome: "Música 3",
+        arquivo: "musicas/musica3.mp3"
+    }
+];
+
+
+const lista = document.getElementById("musicList");
+const player = document.getElementById("audioPlayer");
+const titulo = document.getElementById("musicTitle");
+
+
+musicas.forEach((musica) => {
+
+    const item = document.createElement("div");
+
+    item.className = "song";
+
+    item.innerHTML = `
+        <span class="song-icon">🎵</span>
+        <span class="song-name">${musica.nome}</span>
+    `;
+
+    item.addEventListener("click", () => {
+
+        player.src = musica.arquivo;
+
+        titulo.textContent = musica.nome;
+
+        player.play();
+
+    });
+
+    lista.appendChild(item);
+
+});
